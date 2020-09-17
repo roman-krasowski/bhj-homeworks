8e490e1e6f108e8c43d89928297a5d5b73fb6fe7
roman-krasowski <54819181+roman-krasowski@users.noreply.github.com>
2020-09-18 00:03:22 +0300

    Update task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,11 +1,22 @@
-const showModalWindow = document.querySelector("#modal_main");
-const closeModalWindow = document.querySelectorAll("modal__close");
-const modalSuccess = document.querySelector("modal_success");
+'use strict'
 
-function closeButtonClick() {
-    closeModalWindow = Array.from(closeModalWindow);
-    let displayNone = closeModalWindow[1];
-    displayNone.style.display = "none";
+let modalMain = document.getElementById('modal_main');
+modalMain.classList.add('modal_active');
+
+let modalClose = document.getElementsByClassName('modal__close');
+modalClose = Array.from(modalClose);
+for(let i=0; i<modalClose.length; i++) {
+    modalClose[i].addEventListener('click', function() {
+        modalMain.classList.remove('modal_active');
+    });
+};
+
+let success = document.getElementsByClassName('show-success');
+let modalSuccess = document.getElementById('modal_success');
+success = Array.from(success);
+for (let i=0; i<success.length; i++) {
+    success[i].addEventListener('click', function() {
+        modalMain.classList.remove('modal_active');
+        modalSuccess.classList.add('modal_active');
+    });
 }
-
-closeModalWindow.onclick = closeButtonClick;
