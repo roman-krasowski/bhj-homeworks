40f85aa5de22bfdb085eb2e3c342f389286af143
roman-krasowski <54819181+roman-krasowski@users.noreply.github.com>
2020-09-25 22:49:56 +0300

    Update task.js

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let dropMenu = document.querySelector('.dropdown__list');
+let menu = document.querySelector('.dropdown__value');
+
+//открытие меню
+menu.addEventListener('click', function() {
+        dropMenu.classList.add('dropdown__list_active');
+    });
+
+//добавление выбранного значения в меню
+let menuText = document.querySelectorAll('.dropdown__link');
+menuText = Array.from(menuText);
+for(let i=0; i<menuText.length; i++) {
+    menuText[i].addEventListener('click', function() {
+        menu.textContent = this.textContent;
+        console.log(this.textContent);
+        return false;
+    });
+};
