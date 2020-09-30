32703bac94d3c8f54e4481d53990323c6860a4de
roman-krasowski <54819181+roman-krasowski@users.noreply.github.com>
2020-09-30 23:36:03 +0300

    Update task.js

---
+++ event-object/tabs/task.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let tab = Array.from(document.querySelectorAll('.tab'));
+let content = Array.from(document.querySelectorAll('.tab__content'));
+
+for(let i=0; i<tab.length; i++) {
+
+    let arr = [tab[i].className]; //получаем массив из классов каждого элемента
+    let activeNow = arr.indexOf('tab tab_active'); //определяем, какой элемент сейчас активный
+
+    tab[i].addEventListener('click', function() {
+        tab[activeNow].classList.remove('tab_active');
+        conyent[activeNow].classList.remove('tab__content_active');
+        tab[i].classList.add('tab_active');
+        content[i].classList.add('tab__content_active');
+    });
+};
