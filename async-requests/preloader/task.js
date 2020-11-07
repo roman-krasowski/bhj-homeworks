d22ab29a349fa8b0b429704ef7936db223c6ef7b
roman-krasowski <54819181+roman-krasowski@users.noreply.github.com>
2020-11-07 16:38:51 +0300

    Update task.js

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,18 +1,27 @@
 'use strict'
 
+const loader = document.getElementById('loader');
+
 const fxRate = document.getElementById('items');
+let itemCode = document.getElementsByClassName('item__code')[0];
+let itemValue = document.getElementsByClassName('item__value')[0];
+
+//создаем запрос на данные
 let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
     xhr.send();
+//если запрос выполнен успешно
     xhr.onreadystatechange = function () {
         if(xhr.readyState === 4) {
+            let valuteArray = JSON.parse(xhr.response).response.Valute;
 
-        let data = JSON.parse(this.responseText);
-
-        let fx = data.response.Valute;
-        for(let i=0; i<fx.length; i++) {
-            console.log(i.CharCode);
-            console.log(i.Value);
+//как вставить сгенерированные ниже ноды в HTML?
+            for(let key in valuteArray) {
+                console.log(fxRate.cloneNode(true));
+                itemCode.textContent = key;
+                let amountArray = valuteArray[key];
+                itemValue.textContent = amountArray.Value;
+            }; 
         };
-        }; 
+        loader.classList.remove('loader_active');
     };
