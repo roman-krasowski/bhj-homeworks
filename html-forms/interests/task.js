26f7ff2421c4d31cdaae185ff9dd471c8e290628
roman-krasowski <54819181+roman-krasowski@users.noreply.github.com>
2020-11-09 23:46:53 +0300

    Update task.js

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,28 +1,24 @@
 'use strict'
+let checkbox = Array.from(document.querySelectorAll('.interest__check'));
+let parent;
 
-//let child = Array.from(document.querySelectorAll('ul.interests_active > li > label > input'));
+//находим чекбоксы-потомки
+for(let i=0; i<checkbox.length; i++) {
+    if(checkbox[i].closest('.interests_active')) {
+        //находим родительские чекбоксы
+        parent = checkbox[i].closest('.interests_active');
+        parent = parent.closest('.interest');
+        parent = parent.querySelector('.interest__check');
+        
+        checkbox[i].addEventListener('click', () => {
+            if(checkbox[i].checked = true) {
+                parent.indeterminate = true;
 
-let parent = [document.querySelector('ul > li'), document.querySelector('ul > li').nextElementSibling]; //сдалать циклом
-let child =[];
-
-//определение дочерних элементов
-for (let i=0; i<parent.length; i++) {
-    child = parent[i].getElementsByTagName('input');
-    parent[i] = parent[i].querySelector('label > input');
-};
-
-//добавление true на дочерние чекбоксы при нажатии родительского
-for(let i=0; i<parent.length; i++) {
-    parent[i].addEventListener('click', function() {
-        if(parent[i].checked) {
-            child[1].checked = true;
-        } else {
-            child[i].checked = false;
-        };
-    });;
-};
-
-//добавление true родительский чекбокс при нажатии дочерних
-
-console.log(child);
-console.log(parent);
+            } else if(parent.checked = true) {
+                for(let i=0; i<checkbox.length; i++) {
+                    checkbox[i].checked = true;
+                }
+            } //+вариант, когда нажаты все потомки и родитель должен быть также нажат?
+        }); 
+    }
+}
