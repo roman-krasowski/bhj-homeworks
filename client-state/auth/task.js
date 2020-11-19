ae6b8ab5e4b878e7eb2a940ff23a6565f474db8e
roman-krasowski <54819181+roman-krasowski@users.noreply.github.com>
2020-11-19 22:22:42 +0300

    Update task.js
    localStorage added

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,40 +1,41 @@
 'use strict'
 
 let signin = document.getElementById('signin');
-signin.classList.add('signin_active');
 let signinForm = document.getElementById('signin__form');
 let welcome = document.getElementById('welcome');
 
-//введенные юзером логин и пароль
-let login = document.getElementsByName('login');
-let password = document.getElementsByName('password');
+//проверяет, был ли ранее пользователь авторизован
+if(localStorage.getItem('authUser') !== null) {
+    welcome.classList.add('welcome_active');
+    welcome.textContent += localStorage.getItem('authUser');
+} else {
+    signin.classList.add('signin_active');
+}
 
+//если пользватель не был ранее авторизован
 signinForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    let form = new FormData(signinForm);
 
-    login = Array.from(login)[0].value;
-    password = Array.from(password)[0].value;
-
-//введенные юзером данные передаются в форму    
-    let form = new FormData();
-    form.append('login', login);
-    form.append('password', password);
-    
 //данные формы отпарвляются на сервер
     let xhr = new XMLHttpRequest();
+    console.log(xhr);
         xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php', true);
         xhr.send(form);
         xhr.onreadystatechange = function() {
             if(xhr.readyState === 4) {
                 let response = JSON.parse(xhr.responseText);
                 if(response.success === true) {
-                    console.log(response.user_id);
                     signin.classList.remove('signin_active');
                     welcome.classList.add('welcome_active');
+                    localStorage.removeItem('authUser');
                     welcome.textContent += response.user_id;
+                    localStorage.setItem('authUser', response.user_id);
                 } else {
                     alert('Неверный логин/пароль');
                 }
             }
         }
 });
+
+//localStorage.removeItem('authUser');
