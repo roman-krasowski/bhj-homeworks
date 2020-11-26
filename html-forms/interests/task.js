86cb4a54d050e4eee00d0d3fc6b1d74243691829
roman-krasowski <54819181+roman-krasowski@users.noreply.github.com>
2020-11-26 22:20:29 +0300

    Update task.js

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,24 +1,22 @@
+
 'use strict'
 let checkbox = Array.from(document.querySelectorAll('.interest__check'));
-let parent;
+let element = Array.from(document.getElementsByClassName('interest'));
+let parent = document.getElementsByClassName('interests_active');
 
-//находим чекбоксы-потомки
-for(let i=0; i<checkbox.length; i++) {
-    if(checkbox[i].closest('.interests_active')) {
-        //находим родительские чекбоксы
-        parent = checkbox[i].closest('.interests_active');
-        parent = parent.closest('.interest');
-        parent = parent.querySelector('.interest__check');
-        
-        checkbox[i].addEventListener('click', () => {
-            if(checkbox[i].checked = true) {
-                parent.indeterminate = true;
+let parents =[];
+let childs = [];
 
-            } else if(parent.checked = true) {
-                for(let i=0; i<checkbox.length; i++) {
-                    checkbox[i].checked = true;
-                }
-            } //+вариант, когда нажаты все потомки и родитель должен быть также нажат?
-        }); 
+for(let i=0; i<element.length; i++) {
+    if(element[i].closest('.interests_active')) {
+        childs.push(element[i]);
+        element[i].addEventListener('click', () => {
+            console.log(element[i].closest('.interests_active'));
+        });
+    } else {
+        parents.push(element[i]);
     }
 }
+
+// console.log(childs);
+// console.log(parents);
